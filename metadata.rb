287cd7dcf7513748115dc8d33e9dc0f350e685ae
okisanjp <okisan.jp@gmail.com>
2014-10-30 16:25:02 +0900

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-recipes-centos6-japanese-support'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef-recipes-centos6-japanese-support'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: chef-recipes-centos6-japanese-support
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# language support
+bash 'set timezone' do
+	user "root"
+	code <<-EOS
+		yum -y groupinstall japanese-support
+		mv /etc/localtime /etc/localtime.org; ln -s /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+	EOS
+end
+
+template "/etc/sysconfig/i18n" do
+  mode '0644'
+  owner 'root'
+  group 'root'
+end
